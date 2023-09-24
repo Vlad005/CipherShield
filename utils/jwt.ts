e47f2ff983abe6695cede8e096674a52b4775630
callmecposu <pc.cposu@gmail.com>
2023-09-24 11:17:53 -0400

    finshed auth

---
+++ utils/jwt.ts
@@ -0,0 +1,11 @@
+import jwt, { JwtPayload, Secret } from "jsonwebtoken"
+
+export const createToken = (id: string): string => {
+    let token = jwt.sign({id}, process.env.JWT_SECRET as Secret, {expiresIn: '1h'});
+    return token;
+}
+
+export const decodeToken = (token:string) : JwtPayload => {
+    let decoded = jwt.verify(token, process.env.JWT_TOKEN as Secret) as JwtPayload;
+    return decoded;
+}
